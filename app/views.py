46d2c618ef4b34772b95b55b5ad9139e6f72215e
marieS06 <81250003+marieS06@users.noreply.github.com>
2021-09-10 12:18:38 +0200

    lol

--- app/views.py
+++ app/views.py
@@ -15,76 +15,85 @@ import calendar
 # @app.route est un décorateur de la varibale app qui va encapsuler la fonction index() et acheminer les demande vers cette fonction
 
 
-
 zoneA = [
-    '03','15', '43', '63','07', '26', '38', '73', '74', '01', '42', '69', '25', '39', '70','90', '21',
+    '03', '15', '43', '63', '07', '26', '38', '73', '74', '01', '42', '69', '25', '39', '70', '90', '21',
     '58', '71', '89', '24', '33', '40', '47', '64', '19', '23', '87', '16', '17', '79', '86'
-] 
+]
 
-zoneB =  [
-    '22','29','35','56', '18', '28', '36', '37', '41', '45', '54', '55','57', '88','08', '10','51', '52','67','68', '02', '60',
-    '80', '59', '62', '14','50', '61', '27', '76', '975', '44', '49', '53', '72', '85', '04', '05', '13', '84', '06', '83'
-    ]
+zoneB = [
+    '22', '29', '35', '56', '18', '28', '36', '37', '41', '45', '54', '55', '57', '88', '08', '10', '51', '52', '67', '68', '02', '60',
+    '80', '59', '62', '14', '50', '61', '27', '76', '975', '44', '49', '53', '72', '85', '04', '05', '13', '84', '06', '83'
+]
 
-zoneC = ['77', '93', '94', '75', '78', '91', '92', '95', '11', '30', '34','48', '66', '09', '12', '31', '32', '46', '65', '81', '82']
+zoneC = ['77', '93', '94', '75', '78', '91', '92', '95', '11', '30',
+         '34', '48', '66', '09', '12', '31', '32', '46', '65', '81', '82']
 
-zoneAUTRE = ['2A', '2B', '971', '977', '978', '972', '974', '976', '986', '987', '988', '973']
+zoneAUTRE = ['2A', '2B', '971', '977', '978',
+             '972', '974', '976', '986', '987', '988', '973']
 
 controllers = Blueprint("main", __name__, url_prefix="/")
 
+
 @controllers.route('/')
 def index():
-   
-    return render_template( 'index.html')
+
+    return render_template('index.html')
+
 
 @controllers.route('/dashboard')
 def dashboard():
-  
-   
-    return render_template( 'dashboard.html')
+
+    return render_template('dashboard.html')
+
 
 @controllers.route('/accueil')
 def accueil():
-    
-    return render_template( 'index.html')
+
+    return render_template('index.html')
 
 
 @controllers.route('/formulaire_predict')
-def formulaire_predict():    
-    
-    return render_template( 'formulaire_predict.html')
+def formulaire_predict():
 
-@controllers.route('/predict', methods = ['POST', 'GET'])
+    return render_template('formulaire_predict.html')
+
+
+@controllers.route('/predict', methods=['POST', 'GET'])
 def predict():
-   
+
     dep = request.form['dep']
     agg = int(request.form['agg'])
     atm = int(request.form['atm'])
     lum = int(request.form['lum'])
     date_request = request.form['date']
     cat_time = int(request.form['cat_time'])
+ 
+    vacances = int(request.form['vacances'])
 
-    #convertir le type de date
+  
+
+    # convertir le type de date
     #date = datetime.date(int(date_request[0]), int(date_request[1]), int(date_request[2]))
-    date = datetime.date(int(date_request[0:4]), int(date_request[5:7]), int(date_request[8:]))
+    date = datetime.date(int(date_request[0:4]), int(
+        date_request[5:7]), int(date_request[8:]))
     journee = calendar.day_name[date.weekday()]
-    
+
     print(date)
-   
 
-    
+    model = load('app/static/pro.joblib')
 
-    model = load('app/static/se.joblib')
-    
-    prediction = model.predict([[dep,lum,agg,atm,date,cat_time, journee]])[0]
+    prediction = model.predict(
+        [[dep, lum, agg, atm, date, cat_time, journee, vacances]])[0]
     print(prediction)
 
     return render_template('predict.html', prediction=prediction)
-    
+
+
 @controllers.route('/offline')
-def offline():    
+def offline():
     return render_template("offline.html")
 
+
 @controllers.route('/sw.js')
-def sw():  
+def sw():
     return send_from_directory(current_app.static_folder, "generate-sw.js")
